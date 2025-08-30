cc3facdbf8f0f22a4e9d3c8d23636332a2c3fdc4
George Anastasiou <g-anastasiou@hotmail.com>
2025-08-30 15:11:03 +0200

    New project

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 import * as THREE from "three"
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger);
 
-//     const contents = gsap.utils.toArray(".content");
-//     const text = gsap.utils.toArray(".text");
-//     const imageWrappers = gsap.utils.toArray(".img-wrapper");
+    const contents = gsap.utils.toArray(".content");
+    const text = gsap.utils.toArray(".text");
+    const imageWrappers = gsap.utils.toArray(".img-wrapper");
 
-//     // move first child text slightly upwards
-//     gsap.set(".content:first-child .text", { y: -50 }); 
+    // move first child text slightly upwards
+    gsap.set(".content:first-child .text", { y: -50 }); 
 
-//     const tl = gsap.timeline({
-//         defaults: {ease: "power2.out"},
-//            scrollTrigger: {
-//             trigger: ".container",
-//             pin: true,
-//             start: "top top",
-//             end: `+=${contents.length * 100}%`, // the scroll lenght based on the number of content sections
-//             scrub: 3, // control animation speed: increasing the value -> slower animation 
-//         },
-//     });
+    const tl = gsap.timeline({
+        defaults: {ease: "power2.out"},
+           scrollTrigger: {
+            trigger: ".container",
+            pin: true,
+            start: "top top",
+            end: `+=${contents.length * 100}%`, // the scroll lenght based on the number of content sections
+            scrub: 3, // control animation speed: increasing the value -> slower animation 
+        },
+    });
 
-//     tl.to(imageWrappers[0], { rotate: -3 }, 0);
+    tl.to(imageWrappers[0], { rotate: -3 }, 0);
     
-//     contents.forEach((_,i) => {
-//         if(i === contents.length -1 ) return;
+    contents.forEach((_,i) => {
+        if(i === contents.length -1 ) return;
 
-//         tl.to(text[i], { opacity: 0, duration: 2}, "+=0.5")
-//           .to(
-//             imageWrappers[i + 1],
-//             {
-//                 scale: 1,
-//                 duration: 2,
-//                 y: (i + 1) * 5,
-//                 x: (i + 1) * -5,
-//                 opacity: 1,
-//                 rotate: (i + 1) * 2 * ( i % 2 === 0 ? 1 : -1), // if element from array is mona then rotate from the other side
-//             },
-//             "<"
-//           )
-//           .to(text[i + 1], { opacity: 1, y: -50, duration: 2 }, "<+=0.5");
-//     })
-// });
+        tl.to(text[i], { opacity: 0, duration: 2}, "+=0.5")
+          .to(
+            imageWrappers[i + 1],
+            {
+                scale: 1,
+                duration: 2,
+                y: (i + 1) * 5,
+                x: (i + 1) * -5,
+                opacity: 1,
+                rotate: (i + 1) * 2 * ( i % 2 === 0 ? 1 : -1), // if element from array is mona then rotate from the other side
+            },
+            "<"
+          )
+          .to(text[i + 1], { opacity: 1, y: -50, duration: 2 }, "<+=0.5");
+    })
+});
 
 
 const container = document.getElementById("about-3d");
@@ -110,24 +110,24 @@ animate();
 
 
 // JAPAN TIMELINE
-// gsap.registerPlugin(ScrollTrigger)
-// const photography = document.querySelector("#photography");
-// const gallery = document.querySelectorAll(".gallery img");
-// const japan = document.querySelector(".japan");
+gsap.registerPlugin(ScrollTrigger)
+const photography = document.querySelector("#photography");
+const gallery = document.querySelectorAll(".gallery img");
+const japan = document.querySelector(".japan");
 
-// const tl = gsap.timeline({
-//   defaults: {duration: .5, ease: "power2.out"},
-//   scrollTrigger: {
-//     trigger: photography,
-//     start: "bottom bottom",
-//     end: `${gallery.length * 100}%`,
-//     scrub: 3, 
-//     pin: true
-//   }
-// })
+const tl = gsap.timeline({
+  defaults: {duration: .5, ease: "power2.out"},
+  scrollTrigger: {
+    trigger: photography,
+    start: "bottom bottom",
+    end: `${gallery.length * 100}%`,
+    scrub: 3, 
+    pin: true
+  }
+})
 
-// gallery.forEach((_,i) => {
-//   if(i === gallery.length -1) return
-//   tl.to(gallery[i], { opacity: 1},
-//   )
-// })
+gallery.forEach((_,i) => {
+  if(i === gallery.length -1) return
+  tl.to(gallery[i], { opacity: 1},
+  )
+})
